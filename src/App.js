6f98070cb930349f0dceced039f0639389e9d48f
GodNowoon <59138974+GodNowoon@users.noreply.github.com>
2024-05-15 20:11:30 +0900

    코드 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import './App.css';
+import TodoList from './TodoList'
 
-function App() {
+class App extends Component {
+  state = {                               
+    pageName : ''
+  }
+  componentDidMount () {
+    window.onpopstate =  (event) => {
+      this.onChangePage(event.state)
+    }
+  }
+  onChangePage = pageName => {
+    this.setState({pageName})
+  }
+
+  onClick1 = () => {
+    const pageName = 'archive';
+    window.history.pushState(pageName, '', '/archive');
+    this.onChangePage(pageName);  
+  }
+  
+  onClick2 = () => {
+    const pageName = 'summarize';
+    window.history.pushState(pageName, '', '/summarize');
+    this.onChangePage(pageName)  
+  }
+  render () {
+    const {pageName} = this.state;
+    return (
+      <div>
+        <button onClick={this.onClick1}>Archive</button>
+        <button onClick={this.onClick2}>Summarize</button>
+        {!pageName && <Home/>}              
+        {pageName==='archive'&&<Page1/>}
+        {pageName==='summarize'&&<Page2/>}
+      </div>
+    )
+  }
+}
+function Home(){            
+  return (
+    <div className="container toss-style">
+      <h1>ASMR</h1>
+      <h5>
+        <span>Article</span><br />
+        <span>Summarize</span><br />
+        <span>Memorize</span><br />
+        <span>Recommend</span>
+      </h5>
+  <h3>여기는 메인페이지입니다. 원하는 메뉴를 클릭하세요.</h3>
+  </div>
+  );
+}
+function Page1() {
+  return (
+    <div className="App">
+      <TodoList/>
+    </div>
+  );
+}
+
+
+function Page2() {
   return (
     <div className="container toss-style">
       <h1>ASMR</h1>
@@ -21,5 +82,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
